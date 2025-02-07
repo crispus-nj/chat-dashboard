e2e10a0b36278276e55d0306b1854d77965d333a
crispus-n <crispus@thehillcroftgroup.com>
2025-02-07 10:41:03 +0300

    implemented auth login page design

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
-import { Card, CardContent } from '@/components/ui/card';
 </script>
 
 <template>
-  <div class="flex items-center justify-center min-h-screen bg-gray-100">
-    <div class="w-full max-w-md space-y-5">
-      <img
+  <div class="flex items-center justify-center min-h-screen bg-gray-100 ">
+    <div class="w-full max-w-md">
+      <!-- <img
         class="w-40 object-contain mx-auto"
-        src="@/assets/img/logo.png"
+        src="@/assets/img/belva-digital.png"
         alt="Belva Logo"
-      />
-      <Card>
-        <CardContent class="p-7">
+      /> -->
+      <div>
+        <div class="p-7">
           <slot />
-        </CardContent>
-      </Card>
+        </div>
+      </div>
     </div>
   </div>
 </template>
---
+++ pages/auth/login.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { toTypedSchema } from '@vee-validate/zod';
+import * as z from 'zod';
+
+
+definePageMeta({
+  layout: 'auth',
+});
+
+const formSchema = toTypedSchema(
+  z.object({
+    email: z.string().email().min(4, { message: 'Email is required' }).trim(),
+    password: z.string().min(1, { message: 'Password is required' }).trim(),
+  })
+);
+
+
+</script>
+<template>
+  <section class="">
+    <div class="text-center mb-3">
+    <h1 class="lg:text-2xl text-lg font-bold text-primary">Sign In</h1>
+    <p class="">Access to your dashboard</p>
+  </div>
+  <section>
+    <form class="space-y-5 w-full">
+      <div class="form-group flex flex-col space-y-1">
+        <label for="" class="left-3 text-base font-regular">Email</label>
+        <input
+          type="email"
+          id="email"
+          class="rounded-lg border border-solid bg-white bg-opacity-20 border-zinc-400 h-[50px] px-4"
+        />
+      </div>
+      <div class="form-group flex flex-col space-y-1">
+        <label for="" class="left-3 text-base font-regular">Password</label>
+        <input
+          type="password"
+          id="password"
+          class="rounded-lg border border-solid bg-white bg-opacity-20 border-zinc-400 h-[50px] px-4"
+        />
+      </div>
+      <button class="bg-primary text-white font-medium w-full py-2 rounded-md">Login</button>
+    </form>
+  </section>
+  </section>
+</template>
