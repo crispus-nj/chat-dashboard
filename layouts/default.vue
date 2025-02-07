638e6f8031c895ccfa2190c33b592a78cbb52215
crispus-n <crispus@thehillcroftgroup.com>
2025-02-07 11:44:52 +0300

    updated layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,41 @@
-<script lang="ts" setup></script>
+
+<script lang="ts" setup>
+import { useRouter } from "vue-router";
+const router = useRouter()
+const navigateToOpenConverstions = () => {
+  router.push("/open-conversations")
+}
+
+const navigateToDashboard = () => {
+  router.push("/")
+}
+
+const navigateToUsers = () => {
+  router.push("/users")
+}
+</script>
 
 <template>
-  <div class="p-5">
-    <slot />
+  <div class="flex">
+    <!-- Sidebar -->
+    <aside class="w-64 bg-gray-900 text-white p-5 space-y-5 hidden md:block">
+      <h2 class="text-lg font-semibold mb-3" @click="navigateToDashboard">Dashboard</h2>
+
+      <!-- Open Conversations -->
+      <div>
+        <h3 class="text-sm font-medium uppercase opacity-70" @click="navigateToOpenConverstions">Open Conversations</h3>
+      </div>
+
+      <!-- Users -->
+      <div>
+        <h3 class="text-sm font-medium uppercase opacity-70" @click=navigateToUsers>Users</h3>
+      </div>
+    </aside>
+
+    <!-- Content Area -->
+    <div class="flex-1 p-5">
+      <slot />
+    </div>
   </div>
 </template>
+
---
+++ pages/users/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "default",
+});
+</script>
+
+<template>
+    <p>Users works</p>
+</template>
